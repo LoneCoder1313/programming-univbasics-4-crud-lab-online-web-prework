eedabd35354daa4f9f73ff889b9dae386f921414
Ricky dildy <rdildy92@gmail.com>
2019-09-22 16:42:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  soda_type = 
+  soda_type = ["dr pepper", "coke", "fanta", "gingerale", "mountaindew"]
+  soda_type
 end
 
 def remove_element_from_start_of_array(array)

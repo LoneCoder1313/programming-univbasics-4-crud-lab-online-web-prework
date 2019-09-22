238b480804b23df17cf02df4f8be25a502f26856
Ricky dildy <rdildy92@gmail.com>
2019-09-22 15:49:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,10 +4,9 @@ def create_an_empty_array
 puts "puppies"
 end
 
-numbers = ["1", "2", "3", "4"]
+
 def create_an_array
-  ["1", "2", "3", "4"]
-#=> ["1", "2", "3", "4"]
+  
 end
 
 def add_element_to_end_of_array(array, element)

ee4906e18b61acf7ec674eec92fd8cdf403ff395
Ricky dildy <rdildy92@gmail.com>
2019-09-22 16:42:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   soda_type = ["dr pepper", "coke", "fanta", "gingerale", "mountaindew"]
-  
+  soda_type.unshift("mountaindew")
 end
 
 def remove_element_from_end_of_array(array)
